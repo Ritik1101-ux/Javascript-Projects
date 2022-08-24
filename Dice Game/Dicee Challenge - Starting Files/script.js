17d85104d62add2f7bb984eeb00a71d23f3a5511
Ritik Singh <ritik.singh@novoinvent.com>
2022-08-24 12:34:53 +0530

    second commit

---
+++ Dice Game/Dicee Challenge - Starting Files/script.js
@@ -0,0 +1,23 @@
+const checkWinner=()=>{
+var randomNumber1=Math.floor(Math.random()*6)+1; //math.random give from (0 to 0.9999) we multiply it by 6 we get from (0 to 5) for avoiding decimal we use floor then add 1
+var randomDiceImage1="images/dice"+randomNumber1+".png";
+
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomDiceImage1); 
+
+//var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomDiceImage2="images/dice"+randomNumber2+".png";
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomDiceImage2);
+
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML=" Player 1 Wins";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML=" Player 2 Wins";
+}
+else{
+    document.querySelector("h1").innerHTML=" Draw! "
+}
+}
