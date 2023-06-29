8c8ceff8707675038bdc827d3bc12906b153cc7f
rocky-321 <117520037+rocky-321@users.noreply.github.com>
2023-06-29 19:45:41 +0530

    Update script.js

--- Dice Game/Dicee Challenge - Starting Files/script.js
+++ Dice Game/Dicee Challenge - Starting Files/script.js
@@ -1,7 +1,7 @@
 const checkWinner=()=>{
 var randomNumber1=Math.floor(Math.random()*6)+1; //math.random give from (0 to 0.9999) we multiply it by 6 we get from (0 to 5) for avoiding decimal we use floor then add 1
 var randomDiceImage1="images/dice"+randomNumber1+".png";
-
+console.log("Hello");
 var image1=document.querySelectorAll("img")[0];
 image1.setAttribute("src",randomDiceImage1); 
 
